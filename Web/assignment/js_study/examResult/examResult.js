dab4ec82cd89cdb5e7c2ff1b0b4628972593c8ee
yh <jghff700@naver.com>
2021-11-19 23:20:51 +0900

    Finish examResult assignment

---
+++ Web/assignment/js_study/examResult/examResult.js
@@ -0,0 +1,25 @@
+let hyesoonScore = {
+	'데이터 모델링의 이해': 10,
+	'데이터 모델과 성능': 8,
+	'SQL 기본': 22,
+	'SQL 활용': 18,
+	'SQL 최적화 기본 원리': 20,
+};
+
+let minsoonScore = {
+	'데이터 모델링의 이해': 14,
+	'데이터 모델과 성능': 8,
+	'SQL 기본': 12,
+	'SQL 활용': 4,
+	'SQL 최적화 기본 원리': 16,
+};
+
+function passChecker(scoreObject) {
+	sum = 0;
+  for (lecture in scoreObject) {
+    sum += scoreObject[lecture];
+  }
+};
+
+passChecker(hyesoonScore);
+passChecker(minsoonScore);
