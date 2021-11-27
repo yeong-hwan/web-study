7be80e970a9d42734af9cdd8fba53fa2f9625f0f
yh <jghff700@naver.com>
2021-11-27 09:59:27 +0900

    Add InteractiveJS folder in js_study

---
+++ Web/assignment/js_study/interactiveJS/index.js
@@ -0,0 +1,7 @@
+const myTag = document.getElementById('myNumber');
+console.log(myTag);
+
+const myTags = document.getElementsByClassName('color-btn');
+for (let tag of myTags) {
+  console.log(tag);
+}
