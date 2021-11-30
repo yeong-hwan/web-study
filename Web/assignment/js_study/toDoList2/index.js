0bd4bcc4c99991e1299923fe42d59c99d389902e
yh <jghff700@naver.com>
2021-11-30 16:32:19 +0900

    Finish toDoList2 assignment

--- Web/assignment/js_study/toDoList2/index.js
+++ Web/assignment/js_study/toDoList2/index.js
@@ -3,11 +3,18 @@ const items = toDoList.children;
 
 // 1. updateToDo 함수를 완성해 주세요.
 function updateToDo(event) {
-
+  event.target.classList.toggle('done');
 }
 
-// 2. 반복문을 활용해서 각 li태그에 이벤트 핸들러를 등록해 주세요.
+// items[2].addEventListener('click', function() {
+//   items[2].classList.toggle('done');
+// });
 
+
+// 2. 반복문을 활용해서 각 li태그에 이벤트 핸들러를 등록해 주세요.
+for (let item of items) {
+  item.addEventListener('click', updateToDo);
+}
 
 
 // 테스트 코드
