8452f277d85ae479b439f3040b6e8ed02ca58549
yh <jghff700@naver.com>
2021-11-30 14:38:24 +0900

    Upload toDoList2 assginment Template

---
+++ Web/assignment/js_study/toDoList2/index.js
@@ -0,0 +1,14 @@
+const toDoList = document.querySelector('#to-do-list');
+const items = toDoList.children;
+
+// 1. updateToDo 함수를 완성해 주세요.
+function updateToDo(event) {
+
+}
+
+// 2. 반복문을 활용해서 각 li태그에 이벤트 핸들러를 등록해 주세요.
+
+
+
+// 테스트 코드
+items[2].removeEventListener('click', updateToDo);
