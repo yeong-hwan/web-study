968eb8b293c046948074cfab029ce00cbc242f30
yh <jghff700@naver.com>
2021-11-30 23:12:24 +0900

    Finish toDoList3 assignment

--- Web/assignment/js_study/toDoList3/index.js
+++ Web/assignment/js_study/toDoList3/index.js
@@ -2,11 +2,13 @@ const toDoList = document.querySelector('#to-do-list');
 
 // 1. updateToDo 함수를 완성해 주세요.
 function updateToDo(event) {
-  
+  if (event.target.classList.contains('item')) {
+    event.target.classList.toggle('done');
+  }
 }
 
 // 2. 각 li 태그가 아니라 하나의 태그에만 이벤트 핸들러를 등록해 주세요.
-
+toDoList.addEventListener('click', updateToDo);
 
 // 테스트 코드
 const newToDo = document.createElement('li');
