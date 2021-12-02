b07f123dcf62c9660e81ccecfe85c2587657df1a
yh <jghff700@naver.com>
2021-12-02 14:57:26 +0900

    Finish hyojunHouse assignment

--- Web/assignment/js_study/hyojunHouse/index.js
+++ Web/assignment/js_study/hyojunHouse/index.js
@@ -1,18 +1,20 @@
 // showTitle 함수를 완성해 주세요.
 function showTitle(e) {
-	if (e.target.style.data-title == true) {
-		const new_element_node = document.createElement('span');
-		
+	if (e.target.dataset.title) {
+		const span = document.createElement('span');
+		span.classList.add('.title');
+		span.textContent = e.target.dataset.title;
+		e.target.append(span);
 	}
 
 }
 
-// removeTitle 함수를 완성해 주세요.
 function removeTitle(e) {
-	// 여기에 코드를 작성해 주세요.
-
+	if (e.target.dataset.title) {
+		e.target.lastElementChild.remove();
+	}
 }
 
-// '대상'과 '타입'을 수정해 주세요.
-대상.addEventListener('타입', showTitle);
-대상.addEventListener('타입', removeTitle);
+const map = document.querySelector('.map');
+map.addEventListener('mouseover', showTitle);
+map.addEventListener('mouseout', removeTitle);
