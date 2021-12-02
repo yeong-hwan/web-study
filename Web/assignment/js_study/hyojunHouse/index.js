667860c1543a3ec3e84962fd25263af59f91f9d4
yh <jghff700@naver.com>
2021-12-02 09:50:13 +0900

    Add hyojunHouse assignment

---
+++ Web/assignment/js_study/hyojunHouse/index.js
@@ -0,0 +1,18 @@
+// showTitle 함수를 완성해 주세요.
+function showTitle(e) {
+	if (e.target.style.data-title == true) {
+		const new_element_node = document.createElement('span');
+		
+	}
+
+}
+
+// removeTitle 함수를 완성해 주세요.
+function removeTitle(e) {
+	// 여기에 코드를 작성해 주세요.
+
+}
+
+// '대상'과 '타입'을 수정해 주세요.
+대상.addEventListener('타입', showTitle);
+대상.addEventListener('타입', removeTitle);
