aa425db5e539950ad24dad2f184f4ffb533ac503
yh <jghff700@naver.com>
2022-04-18 00:48:44 +0900

    Upload LAB4 assignment

---
+++ codeit/assignment/js_study/toDoList4/index.js
@@ -0,0 +1,20 @@
+const list = document.querySelector('.list');
+const data = [{
+    title: '자바스크립트 공부하기',
+    isClear: true,
+  }, {
+    title: '쓰레기 분리수거',
+    isClear: false,
+  }, {
+    title: '고양이 밥주기',
+    isClear: true,
+  }, {
+    title: '독서하기',
+    isClear: false,
+  }, {
+    title: '영어 공부하기',
+    isClear: false,
+  }
+];
+
+
