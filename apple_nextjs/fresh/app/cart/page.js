3b7dd08272af23089c82b293de74fe05f01d4066
yh <jghff700@naver.com>
2023-07-13 22:24:59 +0900

    next.js component

--- apple_nextjs/fresh/app/cart/page.js
+++ apple_nextjs/fresh/app/cart/page.js
@@ -1,17 +1,26 @@
+import { age, name } from "./page.js";
+
 export default function Cart() {
   return (
     <div>
       <h4 className="title">Cart</h4>
-      <div className="cart-item">
-        <p>상품명</p>
-        <p>$40</p>
-        <p>1개</p>
-      </div>
-      <div className="cart-item">
-        <p>상품명</p>
-        <p>$40</p>
-        <p>1개</p>
-      </div>
+      <CartItem />
     </div>
   );
 }
+
+// Component
+function CartItem(props) {
+  return (
+    <div className="cart-item">
+      <p>상품명</p>
+      <p>$40</p>
+      <p>1개</p>
+    </div>
+  );
+}
+
+// return 안에는 최상단 태그 하나만
+
+// server component(default), client component 종류 나뉨
+// 'use client' 파일 맨 위에
