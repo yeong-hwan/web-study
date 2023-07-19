f48560045e34bd8b795f97ba5086a78f4555a2af
yh <jghff700@naver.com>
2023-07-19 23:44:00 +0900

    apple next.js POST/GET

--- apple_nextjs/forum/app/list/DetailLink.js
+++ apple_nextjs/forum/app/list/DetailLink.js
@@ -1,9 +1,10 @@
 "use client";
-import { useRouter } from "next/navigation";
+import { useRouter, usePathname } from "next/navigation";
 
 export default function DetailLink() {
   let router = useRouter();
   let a = usePathname();
+
   return (
     <button
       onClick={() => {
---
+++ apple_nextjs/forum/pages/api/test.js
@@ -0,0 +1,16 @@
+import { ObjectId } from "mongodb";
+import { connectDB } from "@/util/database";
+
+export default async function handler(req, res) {
+  const client = await connectDB;
+  const db = client.db("forum");
+  let result = await db.collection("post").find().toArray();
+  // console.log(result);
+
+  if (req.method == "POST") {
+    return res.status(200).json('good status')
+  } else if (req.method == "GET") {
+    return res.status(200).json(result)
+  }
+  
+}
