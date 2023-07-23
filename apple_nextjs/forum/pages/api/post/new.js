ad6f9d5cf93136790fa684c3a45f9ad43410a02d
yh <jghff700@naver.com>
2023-07-23 18:48:16 +0900

    apple next.js update

--- apple_nextjs/forum/pages/api/post/new.js
+++ apple_nextjs/forum/pages/api/post/new.js
@@ -1,6 +1,12 @@
-export default function handler(req, res) {
+import { connectDB } from "@/util/database";
+
+export default async function handler(req, res) {
   // res.status(200).json(req.body)
   if (req.method == "POST") {
-    console.log(req.body)
+    const client = await connectDB;
+    const db = client.db("forum");
+    let result = await db.collection('post').insertOne(req.body) 
+    // console.log(result)
+    res.redirect(302, '/list')
   }
 }
