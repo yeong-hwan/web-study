07ba7d28d9dd3143ec7d992940ea4d0624e6a057
yh <jghff700@naver.com>
2023-07-28 00:14:43 +0900

    next.js POST Try/catch

--- apple_nextjs/forum/app/write/page.js
+++ apple_nextjs/forum/app/write/page.js
@@ -2,25 +2,24 @@ export default function Write() {
   return (
     <div>
       <h4>write article</h4>
-      
-      <form action="/api/test" method="POST"> 
-        {/* only post, get can method at form tag */}
-        <button type="submit">button for post</button>
-      </form>
-      
-      <form action="/api/test" method="GET"> 
-        {/* only post, get can method at form tag */}
-        <button type="submit">button for get</button>
-      </form>
 
       <div className="p-20">
-        <form action="/api/post/new" method="POST"> 
+        <form action="/api/post/new" method="POST">
           <input name="title" placeholder="title"></input>
           <input name="content" placeholder="content"></input>
           <button type="submit">posting</button>
         </form>
       </div>
     </div>
-    
-  )
+  );
 }
+
+// <form action="/api/test" method="POST">
+//   {/* only post, get can method at form tag */}
+//   <button type="submit">button for post</button>
+// </form>
+
+// <form action="/api/test" method="GET">
+//   {/* only post, get can method at form tag */}
+//   <button type="submit">button for get</button>
+// </form>
--- apple_nextjs/forum/pages/api/post/new.js
+++ apple_nextjs/forum/pages/api/post/new.js
@@ -3,10 +3,14 @@ import { connectDB } from "@/util/database";
 export default async function handler(req, res) {
   // res.status(200).json(req.body)
   if (req.method == "POST") {
-    const client = await connectDB;
-    const db = client.db("forum");
-    let result = await db.collection('post').insertOne(req.body) 
-    // console.log(result)
-    res.redirect(302, '/list')
+    if (req.body.title == "") {
+      res.status(500).json("Write title");
+    } else {
+      const client = await connectDB;
+      const db = client.db("forum");
+      let result = await db.collection("post").insertOne(req.body);
+      // console.log(result)
+      res.redirect(302, "/list");
+    }
   }
 }
